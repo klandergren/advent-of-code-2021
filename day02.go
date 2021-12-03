bb8db87a86b29876eb3c72140e0322fbd9596438
Kip Landergren <klandergren@users.noreply.github.com>
2021-12-02 20:24:56 -0800

    day 02: solve part 2

--- day02.go
+++ day02.go
@@ -11,21 +11,21 @@ import (
 )
 
 func main() {
-	parta()
-	partb()
+	part1()
+	part2()
 
 	fmt.Println("done")
 }
 
-type Coordinate struct {
-	X, Depth int
+type Submarine struct {
+	Aim, X, Depth int
 }
 
-func (c *Coordinate) Mult() int {
-	return c.X * c.Depth
+func (sub *Submarine) Mult() int {
+	return sub.X * sub.Depth
 }
 
-func (c *Coordinate) Advance(step string) {
+func (sub *Submarine) AdvanceWithoutAim(step string) {
 	parts := strings.Fields(step)
 
 	if len(parts) != 2 {
@@ -41,18 +41,49 @@ func (c *Coordinate) Advance(step string) {
 
 	switch direction {
 	case "forward":
-		c.X = c.X + magnitude
+		sub.X = sub.X + magnitude
 	case "down":
-		c.Depth = c.Depth + magnitude
+		sub.Depth = sub.Depth + magnitude
 	case "up":
-		c.Depth = c.Depth - magnitude
+		sub.Depth = sub.Depth - magnitude
 	default:
 		log.Fatal("how'd we get here?")
 	}
 
 }
 
-func parta() {
+func (sub *Submarine) AdvanceWithAim(step string) {
+	parts := strings.Fields(step)
+
+	if len(parts) != 2 {
+		log.Fatal("unexpected step format: ", step)
+	}
+
+	direction := parts[0]
+	magnitude, err := strconv.Atoi(parts[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "forward":
+		sub.X = sub.X + magnitude
+		sub.Depth = sub.Depth + sub.Aim*magnitude
+	case "down":
+		sub.Aim = sub.Aim + magnitude
+	case "up":
+		sub.Aim = sub.Aim - magnitude
+	default:
+		log.Fatal("how'd we get here?")
+	}
+
+	//	fmt.Println("step: ", step)
+	//	fmt.Println("sub: %v", sub)
+
+}
+
+func part1() {
 	//	file, err := os.Open("./test-data/day02.txt")
 	file, err := os.Open("./input-data/day02.txt")
 
@@ -64,7 +95,7 @@ func parta() {
 
 	r := bufio.NewReader(file)
 
-	coord := Coordinate{0, 0}
+	sub := Submarine{0, 0, 0}
 
 	for {
 		line, _, err := r.ReadLine()
@@ -73,11 +104,35 @@ func parta() {
 			break
 		}
 
-		coord.Advance(string(line))
+		sub.AdvanceWithoutAim(string(line))
 	}
 
-	fmt.Println("part a: ", coord.Mult())
+	fmt.Println("part 1: ", sub.Mult())
 }
 
-func partb() {
+func part2() {
+	// file, err := os.Open("./test-data/day02.txt")
+	file, err := os.Open("./input-data/day02.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	sub := Submarine{0, 0, 0}
+
+	for {
+		line, _, err := r.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		sub.AdvanceWithAim(string(line))
+	}
+
+	fmt.Println("part 2: ", sub.Mult())
 }
