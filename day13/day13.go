e12991fbb24dee70ae55238a2ceca6969489533b
Kip Landergren <klandergren@users.noreply.github.com>
2021-12-29 21:27:57 -0800

    solve part two by printing and visually inspecting

--- day13/day13.go
+++ day13/day13.go
@@ -1,7 +1,7 @@
 package day13
 
 import (
-	"errors"
+	"fmt"
 	"io"
 
 	"github.com/klandergren/advent-of-code-2021/util"
@@ -19,7 +19,18 @@ func PartOne(reader io.Reader) (int, error) {
 }
 
 func PartTwo(reader io.Reader) (int, error) {
-	return -1, errors.New("not implemented")
+	tp, instructions, err := Load(reader)
+	if err != nil {
+		return -1, err
+	}
+
+	for _, i := range instructions {
+		tp.Apply(i)
+	}
+
+	fmt.Println(tp)
+
+	return -1, nil
 }
 
 func Load(reader io.Reader) (tp *TransparentPaper, instructions []Instruction, err error) {
